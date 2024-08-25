65483700aaa502fd94da35ac8845390fc266d3b9
mcnoelvito <noelvitohu@gmail.com>
2024-08-26 01:05:05 +0700

    register update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'loader', pathMatch: 'full' },
-  { path: 'loader', loadChildren: () => import('./pages/loader/loader.module').then( m => m.LoaderPageModule) }
-  { path: 'login', loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule) },
+  { path: 'loader', loadChildren: () => import('./pages/loader/loader.module').then( m => m.LoaderPageModule) },
+
   {
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
   }
 
 ];
