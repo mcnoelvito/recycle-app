88aa136b8370ee4863e121f3a02a49aaabcde09c
mcnoelvito <noelvitohu@gmail.com>
2024-08-25 23:53:48 +0700

    Add Login pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,11 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'loader', pathMatch: 'full' },
   { path: 'loader', loadChildren: () => import('./pages/loader/loader.module').then( m => m.LoaderPageModule) }
+  { path: 'login', loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule) },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  }
 
 ];
 
