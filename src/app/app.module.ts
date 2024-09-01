0e8e173748e07ba4ddf85d87d82d79550e3d5acb
mcnoelvito <noelvitohu@gmail.com>
2024-09-01 23:46:15 +0700

    add loading component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppStoreModule } from 'src/store/AppStoreModule';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { LoadingComponent } from './components/loading/loading.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    LoadingComponent
+  ],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
     ...AppStoreModule,
     StoreDevtoolsModule.instrument({ maxAge: 25})
