bd920cc2ed1a6cb859a5c9ebf5b4d7e2f579b33e
mcnoelvito <noelvitohu@gmail.com>
2024-09-02 11:02:08 +0700

    auth service update

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,40 +1,43 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from 'src/app/model/user/User';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import * as firebase from 'firebase/compat/app';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private auth: AngularFireAuth) { }
 
   recoverEmailPassword(email: string) : Observable<void> {
     return new Observable<void>(observer => {
-    setTimeout (() => {
-    if (email == "error@email.com"){
-    observer.error({message: "Email not found"});
-    }
-    observer.next();
-    observer. complete();
-    }, 3000);
-  });
+      this.auth.sendPasswordResetEmail(email).then(() => {
+        this.auth.sendPasswordResetEmail(email).then(() => {
+          observer.next();
+          observer.complete();
+        }).catch(error => {
+          observer.error(error);
+          observer.complete();
+        })
+      })
+    })
   }
 
   login(email: string, password: string) : Observable<User> {
     return new Observable<User>(observer => {
-    setTimeout (() => {
-    if (email == "error@email.com"){
-    observer.error({message: "User not found"});
-    observer.next();
-    } else {
-      const user = new User();
-      user.email = email;
-      user.id = "userid";
-      observer.next(user);
-    }
-    observer. complete();
-    }, 3000);
-  });
+    this.auth.sendPasswordResetEmail(email).then(() => {
+      this.auth.sendPasswordResetEmail(email).then(() => {
+        observer.next();
+        observer.complete();
+      }).catch(error => {
+        observer.error(error);
+        observer.complete();
+      })
+    })
+  }
+  
+);
   }
 }
